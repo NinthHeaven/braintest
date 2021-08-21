20581a815d24ce600ee1fa0561db4771dbb712b5
Saul Soto <ssoto1@college.harvard.edu>
2021-08-21 04:00:26 -0400

    heroku deployment changes

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class DefaultConfig(object):
     SECRET_KEY = '~\xa5\xd36;\x8e\x98&\xcc\x00\x93E&\xcen\x91\xe1\xf6v\x19\x87\xd0\xd2\xab'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') or 'sqlite:///' + os.path.join(basedir, 'usernames.db')
     SQLALCHEMY_TRACK_MODIFACTIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
